8c84d9749b5527269ddd1ce471dd0716015c2253
rowanedwards <rowancoope@gmail.com>
2021-05-13 17:32:48 +1000

    Home page fixed

--- wp-content/themes/kirkandkirk/app/Controllers/FrontPage.php
+++ wp-content/themes/kirkandkirk/app/Controllers/FrontPage.php
@@ -7,4 +7,16 @@ use Sober\Controller\Controller;
 class FrontPage extends Controller
 {
   use Partials\ContentBlocks;
+
+  function type(){
+
+    global $post;
+
+    $type = array (
+      'page_type' => get_field('page_type'),
+      'user'      => wp_get_current_user(),
+    );
+
+    return $type;
+  }
 }
