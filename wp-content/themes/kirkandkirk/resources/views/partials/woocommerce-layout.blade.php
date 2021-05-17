ffc0046e3fb0e3fac4c72ca893b1713160c149ff
rowanedwards <rowancoope@gmail.com>
2021-05-17 15:11:56 +1000

    optical fixed when filter is used

---
+++ wp-content/themes/kirkandkirk/resources/views/partials/woocommerce-layout.blade.php
@@ -0,0 +1,61 @@
+
+  {{-- @php
+    do_action('get_header', 'shop');
+    do_action('woocommerce_before_main_content');
+  @endphp --}}
+
+  <div class="breadcrumb-container">
+    <?php woocommerce_breadcrumb();?>
+  </div>
+
+  @include('partials.filter')
+
+  <header class="woocommerce-products-header">
+    <div class="woocommerce-products-header__inner">
+      @if(apply_filters('woocommerce_show_page_title', true))
+        <h1 class="woocommerce-products-header__title page-title">{!! woocommerce_page_title(false) !!}</h1>
+      @endif
+
+      @php
+        do_action('woocommerce_archive_description');
+      @endphp
+    </div>
+  </header>
+
+  @php
+    do_action('get_header', 'shop');
+    do_action('woocommerce_before_main_content');
+  @endphp
+
+
+  @if(woocommerce_product_loop())
+    @php
+      do_action('woocommerce_before_shop_loop');
+      woocommerce_product_loop_start();
+    @endphp
+
+    @if(wc_get_loop_prop('total'))
+      @while(have_posts())
+        @php
+          the_post();
+          do_action('woocommerce_shop_loop');
+          wc_get_template_part('content', 'product');
+        @endphp
+      @endwhile
+    @endif
+
+    @php
+      woocommerce_product_loop_end();
+      do_action('woocommerce_after_shop_loop');
+    @endphp
+  @else
+    @php
+      do_action('woocommerce_no_products_found');
+    @endphp
+  @endif
+
+  @php
+  do_action('woocommerce_after_main_content');
+  do_action('get_sidebar', 'shop');
+  do_action('get_footer', 'shop');
+@endphp
--- wp-content/themes/kirkandkirk/resources/views/woocommerce/archive-product.blade.php
+++ wp-content/themes/kirkandkirk/resources/views/woocommerce/archive-product.blade.php
@@ -21,6 +21,14 @@ the readme will list any important changes.
 
   @if(is_product_category('optical'))
 
+    {{-- Fix for filters --}}
+    @if (strpos($_SERVER['REQUEST_URI'], "optical/?") !== false)
+
+      @include('partials.woocommerce-layout');
+
+    @else
+
+
     <div class="breadcrumb-container">
       <?php woocommerce_breadcrumb();?>
     </div>
@@ -114,69 +122,12 @@ the readme will list any important changes.
 
     @endphp
 
-  @else
-
-    {{-- @php
-      do_action('get_header', 'shop');
-      do_action('woocommerce_before_main_content');
-    @endphp --}}
-
-    <div class="breadcrumb-container">
-      <?php woocommerce_breadcrumb();?>
-    </div>
-
-    @include('partials.filter')
-
-    <header class="woocommerce-products-header">
-      <div class="woocommerce-products-header__inner">
-        @if(apply_filters('woocommerce_show_page_title', true))
-          <h1 class="woocommerce-products-header__title page-title">{!! woocommerce_page_title(false) !!}</h1>
-        @endif
-
-        @php
-          do_action('woocommerce_archive_description');
-        @endphp
-      </div>
-    </header>
-
-    @php
-      do_action('get_header', 'shop');
-      do_action('woocommerce_before_main_content');
-    @endphp
-
-
-    @if(woocommerce_product_loop())
-      @php
-        do_action('woocommerce_before_shop_loop');
-        woocommerce_product_loop_start();
-      @endphp
-
-      @if(wc_get_loop_prop('total'))
-        @while(have_posts())
-          @php
-            the_post();
-            do_action('woocommerce_shop_loop');
-            wc_get_template_part('content', 'product');
-          @endphp
-        @endwhile
-      @endif
-
-      @php
-        woocommerce_product_loop_end();
-        do_action('woocommerce_after_shop_loop');
-      @endphp
-    @else
-      @php
-        do_action('woocommerce_no_products_found');
-      @endphp
     @endif
 
+  @else
+
+    @include('partials.woocommerce-layout');
 
   @endif
 
-  @php
-    do_action('woocommerce_after_main_content');
-    do_action('get_sidebar', 'shop');
-    do_action('get_footer', 'shop');
-  @endphp
 @endsection
